bb61ec03492cd4b198ea7dd6dda092d8fd03c16d
cyrus1127 <cyrus1127@gmail.com>
2021-09-14 00:29:58 +0100

    Initial Commit

---
+++ TechTest_FruitApp_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechTest_FruitApp_swift
+//
+//  Created by Chun Yip Lam on 14/9/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
